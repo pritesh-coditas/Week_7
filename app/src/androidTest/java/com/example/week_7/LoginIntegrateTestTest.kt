964348f468209300f0bfe6c5fc578bc5c4f8afac
Pritesh Nikam <pritesh.nikam@coditas.com>
2022-09-20 10:35:27 +0530

    Integration-Test

--- app/src/androidTest/java/com/example/week_7/LoginIntegrateTestTest.kt
+++ app/src/androidTest/java/com/example/week_7/LoginIntegrateTestTest.kt
@@ -1,5 +1,38 @@
 package com.example.week_7
 
-import org.junit.jupiter.api.Assertions.*
+import android.app.Application
+import android.content.Context
+import android.util.Log
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
 
-internal class LoginIntegrateTestTest
+internal class LoginIntegrateTestTest{
+
+    private lateinit var loginActivityTest:LoginIntegrateTest
+
+    @Before
+    fun doInFirst(){
+        loginActivityTest = LoginIntegrateTest()
+    }
+
+   // @After // doubt?
+
+    @Test
+    fun checkFun(){
+        val context = ApplicationProvider.getApplicationContext<Context>()
+
+        val result = loginActivityTest.checkData(context.getString(R.string.app_name),"Week_7")
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun checkFun2(){
+        //val context = ApplicationProvider.getApplicationContext<Context>()
+
+        val result = loginActivityTest.checkData("Pritesh","nikam")
+        assertThat(result).isTrue()
+    }
+
+}
--- app/src/main/java/com/example/week_7/LoginIntegrateTest.kt
+++ app/src/main/java/com/example/week_7/LoginIntegrateTest.kt
@@ -1,4 +1,16 @@
 package com.example.week_7
 
 class LoginIntegrateTest {
+
+    fun checkData(str1:String,str2:String):Boolean{
+
+        if(str1 == str2){
+            return true
+        }
+
+        if(str1!=str2){
+            return true
+        }
+        return false
+    }
 }
