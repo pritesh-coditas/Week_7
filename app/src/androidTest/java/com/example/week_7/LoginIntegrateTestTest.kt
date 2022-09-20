53a0a2c62e3ae435c7cb6212dcb253239a593158
Pritesh Nikam <pritesh.nikam@coditas.com>
2022-09-20 10:32:12 +0530

    Integration_testing

---
+++ app/src/androidTest/java/com/example/week_7/LoginIntegrateTestTest.kt
@@ -0,0 +1,5 @@
+package com.example.week_7
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class LoginIntegrateTestTest
---
+++ app/src/main/java/com/example/week_7/LoginIntegrateTest.kt
@@ -0,0 +1,4 @@
+package com.example.week_7
+
+class LoginIntegrateTest {
+}
