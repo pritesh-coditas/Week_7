b0c4a2798cb8bdae87ac992b785d207cd323dec4
Pritesh Nikam <pritesh.nikam@coditas.com>
2022-09-21 11:55:18 +0530

    Espresso-test-improved

--- app/src/androidTest/java/com/example/week_7/my_integration_espresso/MyEspressoActivityTest.kt
+++ app/src/androidTest/java/com/example/week_7/my_integration_espresso/MyEspressoActivityTest.kt
@@ -1,43 +1,53 @@
 package com.example.week_7.my_integration_espresso
 
-import androidx.lifecycle.Lifecycle
-import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.example.week_7.R
+import org.hamcrest.Matchers.allOf
+import org.junit.Rule
 import org.junit.Test
-
 
 internal class MyEspressoActivityTest{
 
+/*    @Before
+    fun initScenario(){ // Resumed state indicate activity is running and visible to user.
+        val scenario = ActivityScenario.launch(MyEspressoActivity::class.java)
+        scenario.moveToState(Lifecycle.State.RESUMED)
+    }*/
+
+    @get:Rule
+    var scenario = ActivityScenarioRule(MyEspressoActivity::class.java)
+
     @Test
     fun checkEspressoActivityDisplay(){
-        val scenario = ActivityScenario.launch(MyEspressoActivity::class.java)
         onView(withId(R.id.activity_my_espresso)).check(matches(isDisplayed()))
     }
 
     @Test
     fun checkTextViewVisible(){
-        val scenario = ActivityScenario.launch(MyEspressoActivity::class.java)
-        scenario.moveToState(Lifecycle.State.RESUMED)
         onView(withId(R.id.espresso_TV)).check(matches(isDisplayed()))
     }
 
     @Test
+    fun checkTextInTextView(){
+        onView(withText(R.string.espresso)).check(matches(withText("Espresso")))
+    }
+
+    @Test
+    fun checkEditTextHasErrorTest(){
+        onView(allOf(withId(R.id.espresso_ET), hasErrorText("Enter name")))
+    }
+
+    @Test
     fun checkBtnViewVisible(){
-        val scenario = ActivityScenario.launch(MyEspressoActivity::class.java)
-        scenario.moveToState(Lifecycle.State.RESUMED)
         onView(withId(R.id.gotoSecondActivity_Btn)).check(matches(isDisplayed()))
     }
 
     @Test
     fun checkActivityChange(){
-        val scenario = ActivityScenario.launch(MyEspressoActivity::class.java)
-        scenario.moveToState(Lifecycle.State.RESUMED)
-
         onView(withId(R.id.gotoSecondActivity_Btn)).perform(click())
 
         onView(withId(R.id.second_Activity_layout)).check(matches(isDisplayed()))
