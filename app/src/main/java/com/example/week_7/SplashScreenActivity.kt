802b52bbfdb49e3987a6990a6c2a5a2264021218
Pritesh Nikam <pritesh.nikam@coditas.com>
2022-09-22 12:56:07 +0530

    Hanlder_splash_screen

---
+++ app/src/main/java/com/example/week_7/SplashScreenActivity.kt
@@ -0,0 +1,17 @@
+package com.example.week_7
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+        },4000)
+    }
+}
